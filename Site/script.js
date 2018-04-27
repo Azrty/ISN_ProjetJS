5056c5b55d96045861b8aa5a5e7f701d9545624d
automate59go <automate59go@gmail.com>
2018-04-27 20:25:26 +0200

    Add code
    Ajout du code source du projet

---
+++ Site/script.js
@@ -0,0 +1,573 @@
+window.addEventListener("load",function(){
+/*---------------------------------*/
+/*------------EXERCICE1------------*/
+/*---------------------------------*/
+
+/*---------------------------------*/
+/*-------------Astuce--------------*/
+/*---------------------------------*/
+var astuce1 = document.getElementById("ast1");
+var contentast1 = document.getElementById("divast1");
+var boutontxt = document.getElementById("ast1");
+var controle1 = 0;
+astuce1.addEventListener("click", function(){
+
+
+if(!controle1){
+	contentast1.style.display="block";
+	controle1= 1;
+	boutontxt.innerHTML="<i id='ico1' class='fa fa-times' aria-hidden='true'></i>Fermer l'astuce";
+
+}else{
+	contentast1.style.display="none";
+	controle1= 0;
+	boutontxt.innerHTML ="<i id='ico1' class='fa fa-question' aria-hidden='true'></i>Besoin d'une astuce ?";
+
+}
+});
+/*---------------------------------*/
+/*------------Correction-----------*/
+/*---------------------------------*/
+var corect1 = document.getElementById("corec1");
+var contentcorrec1 = document.getElementById("divcorec1");
+var boutontxt2 = document.getElementById("corec1");
+var controle2 = 0;
+	console.log(boutontxt2)
+corect1.addEventListener("click", function(){
+
+
+if(!controle2){
+	contentcorrec1.style.display="block";
+	controle2= 1;
+	boutontxt2.innerHTML ="<i id='ico1' class='fa fa-times' aria-hidden='true'></i>Fermer la correction";
+
+}else{
+	contentcorrec1.style.display="none";
+	controle2= 0;
+	boutontxt2.innerHTML ="<i id='ico1' class='fa fa-check' aria-hidden='true'></i>Correction";
+}
+});
+/*---------------------------------*/
+/*---------------END---------------*/
+/*---------------------------------*/
+
+/*______________________________________________________________*/
+
+/*---------------------------------*/
+/*------------EXERCICE2------------*/
+/*---------------------------------*/
+
+/*---------------------------------*/
+/*-------------Astuce--------------*/
+/*---------------------------------*/
+var astuce2 = document.getElementById("ast2");
+var contentast2 = document.getElementById("divast2");
+var boutontxt3 = document.getElementById("ast2");
+var controle3 = 0;
+astuce2.addEventListener("click", function(){
+
+
+if(!controle3){
+	contentast2.style.display="block";
+	controle3= 1;
+	boutontxt3.innerHTML ="<i id='ico1' class='fa fa-times' aria-hidden='true'></i>Fermer l'astuce";
+
+}else{
+	contentast2.style.display="none";
+	controle3= 0;
+	boutontxt3.innerHTML ="<i id='ico1' class='fa fa-question' aria-hidden='true'></i>Besoin d'une astuce ?";
+}
+});
+/*---------------------------------*/
+/*------------Correction-----------*/
+/*---------------------------------*/
+var corect2 = document.getElementById("corec2");
+var contentcorrec2 = document.getElementById("divcorec2");
+var boutontxt4 = document.getElementById("corec2");
+var controle4 = 0;
+corect2.addEventListener("click", function(){
+
+
+if(!controle4){
+	contentcorrec2.style.display="block";
+	controle4= 1;
+	boutontxt4.innerHTML ="<i id='ico1' class='fa fa-times' aria-hidden='true'></i>Fermer la correction";
+
+}else{
+	contentcorrec2.style.display="none";
+	controle4= 0;
+	boutontxt4.innerHTML ="<i id='ico1' class='fa fa-check' aria-hidden='true'></i>Correction";
+}
+});
+/*---------------------------------*/
+/*---------------END---------------*/
+/*---------------------------------*/
+
+/*______________________________________________________________*/
+
+/*---------------------------------*/
+/*------------EXERCICE3------------*/
+/*---------------------------------*/
+
+/*---------------------------------*/
+/*-------------Astuce--------------*/
+/*---------------------------------*/
+var astuce3 = document.getElementById("ast3");
+var contentast3 = document.getElementById("divast3");
+var boutontxt5 = document.getElementById("ast3");
+var controle5 = 0;
+astuce3.addEventListener("click", function(){
+
+
+if(!controle5){
+	contentast3.style.display="block";
+	controle5= 1;
+	boutontxt5.innerHTML ="<i id='ico1' class='fa fa-times' aria-hidden='true'></i>Fermer l'astuce";
+
+}else{
+	contentast3.style.display="none";
+	controle5= 0;
+	boutontxt5.innerHTML ="<i id='ico1' class='fa fa-question' aria-hidden='true'></i>Besoin d'une astuce ?";
+}
+});
+/*---------------------------------*/
+/*------------Correction-----------*/
+/*---------------------------------*/
+var corect3 = document.getElementById("corec3");
+var contentcorrec3 = document.getElementById("divcorec3");
+var boutontxt6 = document.getElementById("corec3");
+var controle6 = 0;
+corect3.addEventListener("click", function(){
+
+
+if(!controle6){
+	contentcorrec3.style.display="block";
+	controle6= 1;
+	boutontxt6.innerHTML ="<i id='ico1' class='fa fa-times' aria-hidden='true'></i>Fermer la correction";
+
+}else{
+	contentcorrec3.style.display="none";
+	controle6= 0;
+	boutontxt6.innerHTML ="<i id='ico1' class='fa fa-check' aria-hidden='true'></i>Correction";
+}
+});
+/*---------------------------------*/
+/*---------------END---------------*/
+/*---------------------------------*/
+
+/*______________________________________________________________*/
+
+/*---------------------------------*/
+/*------------EXERCICE4------------*/
+/*---------------------------------*/
+
+/*---------------------------------*/
+/*-------------Astuce--------------*/
+/*---------------------------------*/
+var astuce4 = document.getElementById("ast4");
+var contentast4 = document.getElementById("divast4");
+var boutontxt7 = document.getElementById("ast4");
+var controle7 = 0;
+astuce4.addEventListener("click", function(){
+
+
+if(!controle7){
+	contentast4.style.display="block";
+	controle7= 1;
+	boutontxt7.innerHTML ="<i id='ico1' class='fa fa-times' aria-hidden='true'></i>Fermer l'astuce";
+
+}else{
+	contentast4.style.display="none";
+	controle7= 0;
+	boutontxt7.innerHTML ="<i id='ico1' class='fa fa-question' aria-hidden='true'></i>Besoin d'une astuce ?";
+}
+});
+/*---------------------------------*/
+/*------------Correction-----------*/
+/*---------------------------------*/
+var corect4 = document.getElementById("corec4");
+var contentcorrec4 = document.getElementById("divcorec4");
+var boutontxt8 = document.getElementById("corec4");
+var controle8 = 0;
+corect4.addEventListener("click", function(){
+
+
+if(!controle8){
+	contentcorrec4.style.display="block";
+	controle8= 1;
+	boutontxt8.innerHTML ="<i id='ico1' class='fa fa-times' aria-hidden='true'></i>Fermer la correction";
+
+}else{
+	contentcorrec4.style.display="none";
+	controle8= 0;
+	boutontxt8.innerHTML ="<i id='ico1' class='fa fa-check' aria-hidden='true'></i>Correction";
+}
+});
+/*---------------------------------*/
+/*---------------END---------------*/
+/*---------------------------------*/
+
+/*______________________________________________________________*/
+
+/*---------------------------------*/
+/*------------EXERCICE5------------*/
+/*---------------------------------*/
+
+/*---------------------------------*/
+/*-------------Astuce--------------*/
+/*---------------------------------*/
+var astuce5 = document.getElementById("ast5");
+var contentast5 = document.getElementById("divast5");
+var boutontxt9 = document.getElementById("ast5");
+var controle9 = 0;
+astuce5.addEventListener("click", function(){
+
+
+if(!controle9){
+	contentast5.style.display="block";
+	controle9= 1;
+	boutontxt9.innerHTML ="<i id='ico1' class='fa fa-times' aria-hidden='true'></i>Fermer l'astuce";
+
+}else{
+	contentast5.style.display="none";
+	controle9= 0;
+	boutontxt9.innerHTML ="<i id='ico1' class='fa fa-question' aria-hidden='true'></i>Besoin d'une astuce ?";
+}
+});
+/*---------------------------------*/
+/*------------Correction-----------*/
+/*---------------------------------*/
+var corect5 = document.getElementById("corec5");
+var contentcorrec5 = document.getElementById("divcorec5");
+var boutontxt10 = document.getElementById("corec5");
+var controle10 = 0;
+corect5.addEventListener("click", function(){
+
+
+if(!controle10){
+	contentcorrec5.style.display="block";
+	controle10= 1;
+	boutontxt10.innerHTML ="<i id='ico1' class='fa fa-times' aria-hidden='true'></i>Fermer la correction";
+
+}else{
+	contentcorrec5.style.display="none";
+	controle10= 0;
+	boutontxt10.innerHTML ="<i id='ico1' class='fa fa-check' aria-hidden='true'></i>Correction";
+}
+});
+/*---------------------------------*/
+/*---------------END---------------*/
+/*---------------------------------*/
+
+/*______________________________________________________________*/
+
+/*---------------------------------*/
+/*------------EXERCICE6------------*/
+/*---------------------------------*/
+
+/*---------------------------------*/
+/*-------------Astuce--------------*/
+/*---------------------------------*/
+var astuce6 = document.getElementById("ast6");
+var contentast6 = document.getElementById("divast6");
+var boutontxt11 = document.getElementById("ast6");
+var controle11 = 0;
+astuce6.addEventListener("click", function(){
+
+
+if(!controle11){
+	contentast6.style.display="block";
+	controle11= 1;
+	boutontxt11.innerHTML ="<i id='ico1' class='fa fa-times' aria-hidden='true'></i>Fermer l'astuce";
+
+}else{
+	contentast6.style.display="none";
+	controle11= 0;
+	boutontxt11.innerHTML ="<i id='ico1' class='fa fa-question' aria-hidden='true'></i>Besoin d'une astuce ?";
+}
+});
+/*---------------------------------*/
+/*------------Correction-----------*/
+/*---------------------------------*/
+var corect6 = document.getElementById("corec6");
+var contentcorrec6 = document.getElementById("divcorec6");
+var boutontxt12 = document.getElementById("corec6");
+var controle12 = 0;
+corect6.addEventListener("click", function(){
+
+
+if(!controle12){
+	contentcorrec6.style.display="block";
+	controle12= 1;
+	boutontxt12.innerHTML ="<i id='ico1' class='fa fa-times' aria-hidden='true'></i>Fermer la correction";
+
+}else{
+	contentcorrec6.style.display="none";
+	controle12= 0;
+	boutontxt12.innerHTML ="<i id='ico1' class='fa fa-check' aria-hidden='true'></i>Correction";
+}
+});
+/*---------------------------------*/
+/*---------------END---------------*/
+/*---------------------------------*/
+
+/*______________________________________________________________*/
+
+/*---------------------------------*/
+/*------------EXERCICE7------------*/
+/*---------------------------------*/
+
+/*---------------------------------*/
+/*-------------Astuce--------------*/
+/*---------------------------------*/
+var astuce7 = document.getElementById("ast7");
+var contentast7 = document.getElementById("divast7");
+var boutontxt13 = document.getElementById("ast7");
+var controle13 = 0;
+astuce7.addEventListener("click", function(){
+
+
+if(!controle13){
+	contentast7.style.display="block";
+	controle13= 1;
+	boutontxt13.innerHTML ="<i id='ico1' class='fa fa-times' aria-hidden='true'></i>Fermer l'astuce";
+
+}else{
+	contentast7.style.display="none";
+	controle13= 0;
+	boutontxt13.innerHTML ="<i id='ico1' class='fa fa-question' aria-hidden='true'></i>Besoin d'une astuce ?";
+}
+});
+/*---------------------------------*/
+/*------------Correction-----------*/
+/*---------------------------------*/
+var corect7 = document.getElementById("corec7");
+var contentcorrec7 = document.getElementById("divcorec7");
+var boutontxt14 = document.getElementById("corec7");
+var controle14 = 0;
+corect7.addEventListener("click", function(){
+
+
+if(!controle14){
+	contentcorrec7.style.display="block";
+	controle14= 1;
+	boutontxt14.innerHTML ="<i id='ico1' class='fa fa-times' aria-hidden='true'></i>Fermer la correction";
+
+}else{
+	contentcorrec7.style.display="none";
+	controle14= 0;
+	boutontxt14.innerHTML ="<i id='ico1' class='fa fa-check' aria-hidden='true'></i>Correction";
+}
+});
+/*---------------------------------*/
+/*---------------END---------------*/
+/*---------------------------------*/
+
+/*______________________________________________________________*/
+
+/*---------------------------------*/
+/*------------EXERCICE8------------*/
+/*---------------------------------*/
+
+/*---------------------------------*/
+/*-------------Astuce--------------*/
+/*---------------------------------*/
+var astuce8 = document.getElementById("ast8");
+var contentast8 = document.getElementById("divast8");
+var boutontxt15 = document.getElementById("ast8");
+var controle15 = 0;
+astuce8.addEventListener("click", function(){
+
+
+if(!controle15){
+	contentast8.style.display="block";
+	controle15= 1;
+	boutontxt15.innerHTML ="<i id='ico1' class='fa fa-times' aria-hidden='true'></i>Fermer l'astuce";
+
+}else{
+	contentast8.style.display="none";
+	controle15= 0;
+	boutontxt15.innerHTML ="<i id='ico1' class='fa fa-question' aria-hidden='true'></i>Besoin d'une astuce ?";
+}
+});
+/*---------------------------------*/
+/*------------Correction-----------*/
+/*---------------------------------*/
+var corect8 = document.getElementById("corec8");
+var contentcorrec8 = document.getElementById("divcorec8");
+var boutontxt16 = document.getElementById("corec8");
+var controle16 = 0;
+corect8.addEventListener("click", function(){
+
+
+if(!controle16){
+	contentcorrec8.style.display="block";
+	controle16= 1;
+	boutontxt16.innerHTML ="<i id='ico1' class='fa fa-times' aria-hidden='true'></i>Fermer la correction";
+
+}else{
+	contentcorrec8.style.display="none";
+	controle16= 0;
+	boutontxt16.innerHTML ="<i id='ico1' class='fa fa-check' aria-hidden='true'></i>Correction";
+}
+});
+/*---------------------------------*/
+/*---------------END---------------*/
+/*---------------------------------*/
+
+/*______________________________________________________________*/
+/*---------------------------------*/
+/*---------------NAV---------------*/
+/*---------------------------------*/
+//on récupère les boutons (li)
+var acceuil = document.getElementById("acc");
+var exercice1 = document.getElementById("ex1");
+var exercice2 = document.getElementById("ex2");
+var exercice3 = document.getElementById("ex3");
+var exercice4 = document.getElementById("ex4");
+var exercice5 = document.getElementById("ex5");
+var exercice6 = document.getElementById("ex6");
+var exercice7 = document.getElementById("ex7");
+var exercice8 = document.getElementById("ex8");
+
+//on récupère les sections
+var divaccueil = document.getElementById("accueil");
+var divexe1 = document.getElementById("exo1");
+var divexe2 = document.getElementById("exo2");
+var divexe3 = document.getElementById("exo3");
+var divexe4 = document.getElementById("exo4");
+var divexe5 = document.getElementById("exo5");
+var divexe6 = document.getElementById("exo6");
+var divexe7 = document.getElementById("exo7");
+var divexe8 = document.getElementById("exo8");
+
+
+
+
+
+	//on affiche la page accueil
+acceuil.addEventListener("click", function(){
+	divaccueil.style.display="block";
+	divexe1.style.display="none";
+	divexe2.style.display="none";
+	divexe3.style.display="none";
+	divexe4.style.display="none";
+	divexe5.style.display="none";
+	divexe6.style.display="none";
+	divexe7.style.display="none";
+	divexe8.style.display="none";
+
+});
+//on affiche la page exercice1
+exercice1.addEventListener("click", function(){
+	divaccueil.style.display="none";
+	divexe1.style.display="block";
+	divexe2.style.display="none";
+	divexe3.style.display="none";
+	divexe4.style.display="none";
+	divexe5.style.display="none";
+	divexe6.style.display="none";
+	divexe7.style.display="none";
+	divexe8.style.display="none";
+
+});
+//on affiche la page exercice2
+exercice2.addEventListener("click", function(){
+	divaccueil.style.display="none";
+	divexe1.style.display="none";
+	divexe2.style.display="block";
+	divexe3.style.display="none";
+	divexe4.style.display="none";
+	divexe5.style.display="none";
+	divexe6.style.display="none";
+	divexe7.style.display="none";
+	divexe8.style.display="none";
+
+});
+//on affiche la page exercice3
+exercice3.addEventListener("click", function(){
+	divaccueil.style.display="none";
+	divexe1.style.display="none";
+	divexe2.style.display="none";
+	divexe3.style.display="block";
+	divexe4.style.display="none";
+	divexe5.style.display="none";
+	divexe6.style.display="none";
+	divexe7.style.display="none";
+	divexe8.style.display="none";
+
+});
+//on affiche la page exercice4
+exercice4.addEventListener("click", function(){
+	divaccueil.style.display="none";
+	divexe1.style.display="none";
+	divexe2.style.display="none";
+	divexe3.style.display="none";
+	divexe4.style.display="block";
+	divexe5.style.display="none";
+	divexe6.style.display="none";
+	divexe7.style.display="none";
+	divexe8.style.display="none";
+
+});
+//on affiche la page exercice5
+exercice5.addEventListener("click", function(){
+	divaccueil.style.display="none";
+	divexe1.style.display="none";
+	divexe2.style.display="none";
+	divexe3.style.display="none";
+	divexe4.style.display="none";
+	divexe5.style.display="block";
+	divexe6.style.display="none";
+	divexe7.style.display="none";
+	divexe8.style.display="none";
+
+});
+//on affiche la page exercice6
+exercice6.addEventListener("click", function(){
+	divaccueil.style.display="none";
+	divexe1.style.display="none";
+	divexe2.style.display="none";
+	divexe3.style.display="none";
+	divexe4.style.display="none";
+	divexe5.style.display="none";
+	divexe6.style.display="block";
+	divexe7.style.display="none";
+	divexe8.style.display="none";
+
+});
+//on affiche la page exercice7
+exercice7.addEventListener("click", function(){
+	divaccueil.style.display="none";
+	divexe1.style.display="none";
+	divexe2.style.display="none";
+	divexe3.style.display="none";
+	divexe4.style.display="none";
+	divexe5.style.display="none";
+	divexe6.style.display="none";
+	divexe7.style.display="block";
+	divexe8.style.display="none";
+
+});
+//on affiche la page exercice8
+exercice8.addEventListener("click", function(){
+	divaccueil.style.display="none";
+	divexe1.style.display="none";
+	divexe2.style.display="none";
+	divexe3.style.display="none";
+	divexe4.style.display="none";
+	divexe5.style.display="none";
+	divexe6.style.display="none";
+	divexe7.style.display="none";
+	divexe8.style.display="block";
+
+});
+/*---------------------------------*/
+/*-------------NAV-END-------------*/
+/*---------------------------------*/
+
+/*---------------------------------*/
+/*---------------QCM---------------*/
+/*---------------------------------*/
+
+});
